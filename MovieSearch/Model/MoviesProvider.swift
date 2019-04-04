06521f0e2e568f7bcde8b9bbee2347b730345e32
Edwin Boyko <>
2019-04-04 17:02:31 +0100

    add model classes

---
+++ MovieSearch/Model/MoviesProvider.swift
@@ -0,0 +1,9 @@
+//
+//  MoviesProvider.swift
+//  MovieSearch
+//
+//  Created by Edwin Boyko on 04/04/2019.
+//  Copyright © 2019 Edwin Boyko. All rights reserved.
+//
+
+import Foundation
---
+++ MovieSearch/Model/SearchResponseModel.swift
@@ -0,0 +1,35 @@
+//
+//  SearchResponseModel.swift
+//  MovieSearch
+//
+//  Created by Edwin Boyko on 04/04/2019.
+//  Copyright © 2019 Edwin Boyko. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResponseModel: Decodable {
+    
+    let page: Int
+    let totalResults: Int
+    let totalPages: Int
+    let results: [Movie]
+}
+
+struct Movie: Decodable {
+
+    let voteCount: Int
+    let id: Int
+    let video: Bool
+    let voteAverage: Int
+    let title: String
+    let popularity: Float
+    let posterPath: String
+    let originalLanguage: String
+    let originalTitle: String
+    let genreIds: [Int]
+    let backdropPath: String
+    let adult: Bool
+    let overview: String
+    let releaseDate: Date
+}
