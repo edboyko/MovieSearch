5de118e2c50ee91d5ef5df5b3ed04651edeeb26e
Edwin Boyko <>
2019-04-04 16:53:14 +0100

    initial commit

---
+++ MovieSearch/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieSearch
+//
+//  Created by Edwin Boyko on 04/04/2019.
+//  Copyright © 2019 Edwin Boyko. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
